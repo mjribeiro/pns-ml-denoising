53e4d34ac3c374a67fd19795d230cd4c8e20bb32
Mafalda <me@mafalda.dev>
2022-10-25 16:59:37 +0100

    WIP debugging VAE model, found Gumbel-Softmax and size of latent space are main issues

--- models/vae_model.py
+++ models/vae_model.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 # Based on: https://github.com/Jackson-Kang/Pytorch-VAE-tutorial/blob/master/01_Variational_AutoEncoder.ipynb
 class CoordinateEncoder(nn.Module):
     def __init__(self):
@@ -30,11 +31,6 @@ class Encoder(nn.Module):
         #                                  padding=math.floor(kernel_size/2), 
         #                                  stride=1))
         self.layers.append(nn.Conv1d(in_channels=1, 
-                                     out_channels=16, 
-                                     kernel_size=kernel_size, 
-                                     padding=math.floor(kernel_size/2), 
-                                     stride=1))
-        self.layers.append(nn.Conv1d(in_channels=16, 
                                      out_channels=32, 
                                      kernel_size=kernel_size, 
                                      padding=math.floor(kernel_size/2), 
@@ -44,6 +40,11 @@ class Encoder(nn.Module):
                                      kernel_size=kernel_size, 
                                      padding=math.floor(kernel_size/2), 
                                      stride=1))
+        # self.layers.append(nn.Conv1d(in_channels=32, 
+        #                              out_channels=30, 
+        #                              kernel_size=kernel_size, 
+        #                              padding=math.floor(kernel_size/2), 
+        #                              stride=1))
 
         self.maxpool    = nn.MaxPool1d(kernel_size=self.pool_step, stride=self.pool_step)
         self.leaky_relu = nn.LeakyReLU()
@@ -56,7 +57,12 @@ class Encoder(nn.Module):
         h_ = x
         for layer in self.layers:
             h_   = self.dropout(self.leaky_relu(self.maxpool(layer(h_))))
-        return h_
+
+        latent_size = h_.shape[1]
+        data_length = h_.shape[2]
+        h_ = h_.view(h_.shape[0], h_.shape[1] * h_.shape[2])
+        
+        return h_, latent_size, data_length
         # h_ = x
         # for layer in self.layers:
         #     h_   = F.relu(self.maxpool(layer(h_)))
@@ -80,11 +86,6 @@ class Decoder(nn.Module):
         #                                           padding=0, 
         #                                           stride=self.pool_step))
         self.layers.append(nn.ConvTranspose1d(in_channels=1, 
-                                              out_channels=16,
-                                              kernel_size=self.pool_step, 
-                                              padding=0, 
-                                              stride=self.pool_step))
-        self.layers.append(nn.ConvTranspose1d(in_channels=16, 
                                               out_channels=32,
                                               kernel_size=self.pool_step, 
                                               padding=0, 
@@ -94,6 +95,11 @@ class Decoder(nn.Module):
                                               kernel_size=self.pool_step, 
                                               padding=0, 
                                               stride=self.pool_step))
+        # self.layers.append(nn.ConvTranspose1d(in_channels=32, 
+        #                                       out_channels=1,
+        #                                       kernel_size=self.pool_step, 
+        #                                       padding=0, 
+        #                                       stride=self.pool_step))
 
         self.dropout    = nn.Dropout(0.05)
         self.leaky_relu = nn.LeakyReLU()
@@ -119,25 +125,33 @@ class CoordinateVAEModel(nn.Module):
         self.Decoder = Decoder
 
         self.epoch = 0
+        self.tau = 0.1
+
 
     def forward(self, x):
         # Get logits from encoder
-        logits = self.Encoder(x)
+        logits, latent_size, data_length = self.Encoder(x)
 
-        # # Gumbel-Softmax activation on the latent space
-        # tau = 2*math.exp(-0.0003*self.epoch)
+        # Gumbel-Softmax activation on the latent space
+        self.tau = 2*math.exp(-0.0003*self.epoch)
 
-        # if self.Encoder.training == True:
-        #     y_hard = F.gumbel_softmax(logits, tau=tau, hard=True)
-        #     y_soft = F.gumbel_softmax(logits, tau=tau, hard=False)
-        #     onehot = y_hard - y_soft.detach() + y_soft
-        #     # onehot = F.gumbel_softmax(logits, tau=tau, hard=True)
-        # else:
-        #     y_hard = F.gumbel_softmax(logits, tau=0.1, hard=True)
-        #     y_soft = F.gumbel_softmax(logits, tau=0.1, hard=False)
-        #     onehot = y_hard - y_soft.detach() + y_soft
-        #     # onehot = F.gumbel_softmax(logits, tau=0.1, hard=True)
+        # onehot = F.gumbel_softmax(logits, tau=self.tau, hard=False)
 
-        # # Get output from decoder
-        # return self.Decoder(onehot)
-        return self.Decoder(logits)
+        if self.Encoder.training == True:
+            y_hard = F.gumbel_softmax(logits, tau=self.tau, hard=True)
+            y_soft = F.gumbel_softmax(logits, tau=self.tau, hard=False)
+            onehot = y_hard - y_soft.detach() + y_soft
+            # onehot = F.gumbel_softmax(logits, tau=tau, hard=True)
+        else:
+            y_hard = F.gumbel_softmax(logits, tau=0.1, hard=True)
+            y_soft = F.gumbel_softmax(logits, tau=0.1, hard=False)
+            onehot = y_hard - y_soft.detach() + y_soft
+            # onehot = F.gumbel_softmax(logits, tau=0.1, hard=True)
+        
+        onehot = onehot.view(onehot.shape[0], latent_size, data_length)
+        
+        # Get output from decoder
+        if self.Encoder.training == True:
+            return self.Decoder(onehot)
+        else:
+            return self.Decoder(onehot), onehot
--- toy_problem.py
+++ toy_problem.py
@@ -1,6 +1,7 @@
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,22 +15,39 @@ from models.vae_model import *
 
 # --- Create sine wave
 fs = 30000
-f = 500
-samples = 256
-x = np.sin(2 * np.pi * f * np.arange(samples) / fs)
+f = 10
+samples = 10000
+num_repeats = 1
 
-# --- Extra: add noise
-# noise_levels = np.arange(0.1, 0.6, 0.1)
-# input_data = np.zeros((len(noise_levels), samples))
+x = np.zeros((num_repeats, samples))
 
-# for i in range(len(noise_levels)):
-#     input_data[i] = x + np.random.normal(0, noise_levels[i], samples)
+for i in range(num_repeats):
+    x[i, :] = np.sin(2 * np.pi * f * np.arange(samples) / fs)
 
-# input_data = np.asarray(input_data)
-# time_samples = np.where(input_data==input_data.min())
 
-# --- Plot resulting sine waveform
-# plt.plot(np.arange(samples), x)
+x_flat = x.flatten()
+
+# Take smaller windows from main sine signal
+samples = 512
+x_windows = []
+for i in range(0, len(x_flat) - samples, samples):
+    x_windows.append(x_flat[i:i+samples])
+
+# Store in dataloaders
+train_dataloader = DataLoader(x_windows, batch_size=8, shuffle=True)
+test_dataloader = DataLoader(x_windows, batch_size=1, shuffle=False)
+
+# # --- Add overlap
+# overlap_amount = 0.5
+# overlapping_x = []
+# step_size = int(samples * overlap_amount)
+# overlapping_x = []
+
+# for step in range(0, len(x_flat) - step_size, step_size):
+#     overlapping_x.append(x_flat[step:step+int(step_size/overlap_amount)])
+
+# # --- Plot resulting sine waveform
+# plt.plot(np.arange(samples * num_windows), x.flatten())
 # plt.xlabel('Samples')
 # plt.ylabel('Voltage (V)')
 # plt.show()
@@ -41,8 +59,8 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # Weights&Biases initialisation
 wandb.init(project="PNS Denoising",
            config = {
-              "learning_rate": 0.01,
-              "epochs": 1000,
+              "learning_rate": 1,
+              "epochs": 500,
               "batch_size": 1,
               "kernel_size": 3,
               "mse_weight": 1})
@@ -50,8 +68,8 @@ wandb.init(project="PNS Denoising",
 config = wandb.config
 
 print("Setting up coordinate VAE model...")
-encoder = Encoder(input_dim=1, latent_dim=1, kernel_size=config.kernel_size, num_layers=3, pool_step=2, device=device)
-decoder = Decoder(latent_dim=1, output_dim=1, kernel_size=config.kernel_size, num_layers=3, pool_step=2, device=device)
+encoder = Encoder(input_dim=1, latent_dim=1, kernel_size=config.kernel_size, num_layers=2, pool_step=2, device=device)
+decoder = Decoder(latent_dim=1, output_dim=1, kernel_size=config.kernel_size, num_layers=2, pool_step=2, device=device)
 model = CoordinateVAEModel(Encoder=encoder, Decoder=decoder)
 
 # --- Hyperparameter setup
@@ -78,42 +96,36 @@ model.train()
 if torch.cuda.is_available():
     model.cuda()
 
-# x = torch.from_numpy(input_data).float().to(device)
-x = torch.from_numpy(x).float().to(device)
-x = torch.unsqueeze(x, 0)
-x = torch.unsqueeze(x, 0) # Expanding to (B, C, L) format (B=batch size, C=channels, L=length)
-x.requires_grad = True
-
 best_loss = 99999.0
 
 for epoch in range(config.epochs):
     overall_loss = 0
     model.epoch = epoch + 1
 
-    optimizer.zero_grad()
-
-    x_hat = model(x)
-    loss = loss_function(x, x_hat, mse_weight=config.mse_weight)
-    
-    overall_loss += loss.item()*len(x)
-    
-    loss.backward()
-
-    # print("-----------------------------------------------------")
-    # for i in range(2):
-    #     print(f"Encoder weights in layer {i}: {model.Encoder.layers[i].weight}")
-    #     print(f"Decoder weights in layer {i}: {model.Decoder.layers[i].weight}")
-    #     print(f"Encoder grad for weights in layer {i}: {model.Encoder.layers[i].weight.grad}")
-    #     print(f"Decoder grad for weights in layer {i}: {model.Decoder.layers[i].weight.grad}")
-    #     print("\n ----- \n")
+    for batch_idx, data in enumerate(train_dataloader):
+        data = data.to(device).float()
+        data = data.unsqueeze(1) # Expanding to (B, C, L)
         
-    optimizer.step()
-        
-    print("\tEpoch", epoch + 1, "complete!", "\tLoss: ", loss.item())
-    wandb.log({"Epoch loss": loss.item()})
+        optimizer.zero_grad()
 
-    if loss < best_loss:
-        best_model = copy.deepcopy(model)
+        x_hat = model(data)
+        loss = loss_function(data, x_hat, mse_weight=config.mse_weight)
+        
+        overall_loss += loss.item()*len(data)
+        
+        loss.backward()
+
+        # # Uncomment to print out model parameters (check if gradients are there)
+        # for param in model.parameters():
+        #     print(param.grad)
+            
+        optimizer.step()
+
+        if loss < best_loss:
+            best_model = copy.deepcopy(model)
+            
+    print("\tEpoch", epoch + 1, "complete!", "\tAverage Loss: ", overall_loss / (batch_idx*8))
+    wandb.log({"loss": overall_loss / (batch_idx*8)})
         
 print("Finished!")
 
@@ -122,9 +134,20 @@ best_model.eval()
 
 with torch.no_grad():
     best_model.training = False
-    x_hat = best_model(x)
+    x_hats = np.zeros((len(x_windows), samples))
+    onehots = np.zeros((len(x_windows), int(samples/4)))
+    for idx, test_data in enumerate(test_dataloader):
+        test_data = test_data.unsqueeze(1).to(device).float()
+        x_hat, onehot = best_model(test_data)
 
-# x = x.view(16, 1, input_dim)
-plt.plot(x.cpu().detach().numpy()[0][0])
-plt.plot(x_hat.cpu()[0][0])
+        # Convert to numpy and send to cpu
+        x_hat = x_hat.cpu().numpy()
+        onehot = onehot.cpu().numpy()
+
+        # Store in x_hats
+        x_hats[idx, :] = x_hat
+        onehots[idx, :] = onehot
+
+plt.plot(x.flatten())
+plt.plot(x_hats.flatten())
 plt.show()
