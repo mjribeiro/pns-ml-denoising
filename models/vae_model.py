7ff3168b3ddf987afd8a859253893ae3157bb75a
Mafalda <me@mafalda.dev>
2022-10-28 17:34:05 +0100

    New gumbel-softmax implementation, and trying to address posterior collapse issue..

--- models/vae_model.py
+++ models/vae_model.py
@@ -3,6 +3,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+no_cuda = False
+is_cuda = not no_cuda and torch.cuda.is_available()
+
+
+# Source: https://neptune.ai/blog/gumbel-softmax-loss-function-guide-how-to-implement-it-in-pytorch
+def sample_gumbel(shape, eps=1e-20):
+    U = torch.rand(shape)
+    if is_cuda:
+        U = U.cuda()
+    return -torch.log(-torch.log(U + eps) + eps)
+
+
+def gumbel_softmax_sample(logits, temperature):
+    y = logits + sample_gumbel(logits.size())
+    return F.softmax(y / temperature, dim=-1)
+
+
+def gumbel_softmax(latent_dim, categorical_dim, logits, temperature, hard=False):
+    """
+    ST-gumple-softmax
+    input: [*, n_class]
+    return: flatten --> [*, n_class] an one-hot vector
+    """
+    y = gumbel_softmax_sample(logits, temperature)
+    
+    if not hard:
+        return y.view(-1, latent_dim, categorical_dim)
+
+    shape = y.size()
+    _, ind = y.max(dim=-1)
+    y_hard = torch.zeros_like(y).view(-1, shape[-1])
+    y_hard.scatter_(1, ind.view(-1, 1), 1)
+    y_hard = y_hard.view(*shape)
+    # Set gradients w.r.t. y_hard gradients w.r.t. y
+    y_hard = (y_hard - y).detach() + y
+    return y_hard.view(-1, latent_dim, categorical_dim)
+
 
 # Based on: https://github.com/Jackson-Kang/Pytorch-VAE-tutorial/blob/master/01_Variational_AutoEncoder.ipynb
 class CoordinateEncoder(nn.Module):
@@ -16,17 +53,19 @@ class CoordinateEncoder(nn.Module):
 
 class Encoder(nn.Module):
     
-    def __init__(self, input_dim, latent_dim, kernel_size, num_layers, pool_step, device):
+    def __init__(self, input_dim, latent_dim, kernel_size, num_layers, pool_step, batch_size, device):
         super(Encoder, self).__init__()
 
         self.device = device
 
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
         self.pool_step = pool_step
         self.num_layers = num_layers
         self.layers = nn.ModuleList()
 
-        in_channels = [2, 32]
-        out_channels = [32, 32]
+        in_channels = [2, 64, 128]
+        out_channels = [64, 128, latent_dim]
         
         for i in range(self.num_layers):
             self.layers.append(nn.Conv1d(in_channels=in_channels[i], 
@@ -39,25 +78,25 @@ class Encoder(nn.Module):
         self.leaky_relu = nn.LeakyReLU()
         self.dropout    = nn.Dropout(0.05)
 
-        self.fc_mu = nn.Linear(32*32, 30)
-        self.fc_logvar = nn.Linear(32*32, 30)
+        # self.fc_mu = nn.Linear(batch_size*out_channels[-1]*int(input_dim/(2**num_layers)), latent_dim)
+        # self.fc_logvar = nn.Linear(batch_size*out_channels[-1]*int(input_dim/(2**num_layers)), latent_dim)
 
     
     def forward(self, x):
-        batch, _, _ = x.shape
         h_ = x
         for layer in self.layers:
             h_   = self.dropout(self.leaky_relu(self.maxpool(layer(h_))))
 
-        h_latent = h_.reshape(batch, -1)
-
-        mu = self.fc_mu(h_latent)
-        logvar = self.fc_logvar(h_latent)
+        q = h_.view(h_.shape[0], self.latent_dim*h_.shape[2])
+        
+        # mu = self.fc_mu(h_latent)
+        # logvar = self.fc_logvar(h_latent)
 
         # I think this sort of "flattening" helps a bit with the onehot?
         # h_ = h_.view(h_.shape[0], latent_size * data_length)
         
-        return h_latent, mu, logvar
+        # return h_latent, mu, logvar
+        return q, h_, self.latent_dim, h_.shape[2]
 
 
 class Decoder(nn.Module):
@@ -66,12 +105,14 @@ class Decoder(nn.Module):
         
         self.device = device
         
+        self.latent_dim = latent_dim
+        self.output_dim = output_dim
         self.pool_step = pool_step
         self.num_layers = num_layers
         self.layers = nn.ModuleList()
 
-        in_channels = [30, 32]
-        out_channels = [32, 2]
+        in_channels = [latent_dim, 128, 64]
+        out_channels = [128, 64, 2]
 
         for i in range(self.num_layers):
             self.layers.append(nn.ConvTranspose1d(in_channels=in_channels[i], 
@@ -85,17 +126,17 @@ class Decoder(nn.Module):
         self.tanh       = nn.Tanh()
 
         # TODO: See if these fully connected layers are needed to getting right input shape again
-        self.fc_up = nn.Linear(30, 32*30)
+        # self.fc_up = nn.Linear(latent_dim, int(output_dim/(2**num_layers))*latent_dim)
         
 
     def forward(self, x):
         h = x
         
         # FC layer to get original downsampled dims
-        h = self.fc_up(h)
+        # h = self.fc_up(h)
 
         # Reshape for conv1d
-        h = h.reshape(-1, 30, 32)
+        # h = h.reshape(-1, self.latent_dim, int(self.output_dim/(2**self.num_layers)))
 
         for layer_idx in range(self.num_layers-1):
             h   = self.leaky_relu(self.dropout(self.layers[layer_idx](h)))
@@ -126,29 +167,28 @@ class CoordinateVAEModel(nn.Module):
     
     def forward(self, x):
         # Get logits from encoder
-        latent_data, mu, logvar = self.Encoder(x)
+        q, q_y, latent_dim, categorical_dim = self.Encoder(x)
 
-        z = self.sampler(mu, logvar)
+        # Gumbel-Softmax activation on the latent space
+        self.tau = 2*math.exp(-0.0003*self.epoch)
 
-        # # Gumbel-Softmax activation on the latent space
-        # self.tau = 2*math.exp(-0.0003*self.epoch)
+        # z = self.sampler(mu, logvar)
+        # q_y = q.view(q.shape[0], latent_dim, categorical_dim)
+        z = gumbel_softmax(latent_dim, categorical_dim, q_y, self.tau, hard=True)
 
-        if self.training == True:
-            y_hard = F.gumbel_softmax(z, tau=self.tau, hard=True)
-            y_soft = F.gumbel_softmax(z, tau=self.tau, hard=False)
-            z = y_hard - y_soft.detach() + y_soft
-            # latent_data = F.gumbel_softmax(logits, tau=tau, hard=True)
-        else:
-            y_hard = F.gumbel_softmax(z, tau=0.1, hard=True)
-            y_soft = F.gumbel_softmax(z, tau=0.1, hard=False)
-            z = y_hard - y_soft.detach() + y_soft
-            # latent_data = F.gumbel_softmax(logits, tau=0.1, hard=True)
+        # if self.training == True:
+        #     y_hard = F.gumbel_softmax(z, tau=self.tau, hard=True)
+        #     y_soft = F.gumbel_softmax(z, tau=self.tau, hard=False)
+        #     z = y_hard - y_soft.detach() + y_soft
+        #     # latent_data = F.gumbel_softmax(logits, tau=tau, hard=True)
+        # else:
+        #     y_hard = F.gumbel_softmax(z, tau=0.1, hard=True)
+        #     y_soft = F.gumbel_softmax(z, tau=0.1, hard=False)
+        #     z = y_hard - y_soft.detach() + y_soft
+        #     # latent_data = F.gumbel_softmax(logits, tau=0.1, hard=True)
         
         # See comment in encoder about flattening
         # latent_data = latent_data.view(onehot.shape[0], latent_size, data_length)
         
         # Get output from decoder
-        if self.training == True:
-            return self.Decoder(z)
-        else:
-            return self.Decoder(z), z
+        return self.Decoder(z), F.softmax(q_y, dim=-1).reshape(*q.size()), categorical_dim
--- toy_problem.py
+++ toy_problem.py
@@ -7,24 +7,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 import wandb
 
-from torch.optim import Adam, SGD
+from torch.optim import AdamW, SGD
 from torch.utils.data import DataLoader
+from torchsummary import summary
 
 # Local imports
 from models.vae_model import *
 
 # --- Create sine wave
 fs = 30000
-f = 10
+f = 7
 total_samples = 10000
 num_repeats = 1
 
 x = np.zeros((num_repeats, total_samples))
 x_delayed = np.zeros((num_repeats, total_samples))
 
+add_noise = True
+
 for i in range(num_repeats):
     x[i, :] = np.sin(2 * np.pi * f * np.arange(total_samples) / fs)
-    x_delayed[i, :] = np.roll(x[i, :], 1000)
+    x_delayed[i, :] = np.roll(x[i, :], 3000)
+
+    if add_noise:
+        x[i, :] = x[i, :] + np.random.normal(0, 0.08, total_samples)
+        x_delayed[i, :] = x_delayed[i, :] + np.random.normal(0, 0.08, total_samples)
 
 x_flat = x.flatten()
 x_delayed_flat = x_delayed.flatten()
@@ -34,8 +41,18 @@ x_delayed_flat = x_delayed.flatten()
 # plt.plot(x_delayed_flat)
 # plt.show()
 
+samples = 256
+
+# # --- Add overlap
+# overlap_amount = 0.5
+# overlapping_x = []
+# step_size = int(samples * overlap_amount)
+# overlapping_x = []
+
+# for step in range(0, len(x_flat) - step_size, step_size):
+#     overlapping_x.append(x_flat[step:step+int(step_size/overlap_amount)])
+
 # Take smaller windows from main sine signal
-samples = 128
 x_windows = []
 for i in range(0, len(x_flat) - samples, samples):
     x_windows.append(np.asarray([x_flat[i:i+samples], x_delayed_flat[i:i+samples]]))
@@ -47,25 +64,6 @@ x_windows = np.asarray(x_windows)
 # plt.plot(x_windows[10, 1, :])
 # plt.show()
 
-# Store in dataloaders
-train_dataloader = DataLoader(x_windows, batch_size=8, shuffle=True)
-test_dataloader = DataLoader(x_windows, batch_size=1, shuffle=False)
-
-# # --- Add overlap
-# overlap_amount = 0.5
-# overlapping_x = []
-# step_size = int(samples * overlap_amount)
-# overlapping_x = []
-
-# for step in range(0, len(x_flat) - step_size, step_size):
-#     overlapping_x.append(x_flat[step:step+int(step_size/overlap_amount)])
-
-# # --- Plot resulting sine waveform
-# plt.plot(np.arange(samples * num_windows), x.flatten())
-# plt.xlabel('Samples')
-# plt.ylabel('Voltage (V)')
-# plt.show()
-
 # --- Set up model
 # Select GPU if available
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -73,33 +71,59 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # Weights&Biases initialisation
 wandb.init(project="PNS Denoising",
            config = {
-              "learning_rate": 0.0005,
-              "epochs": 5000,
-              "batch_size": 1,
-              "kernel_size": 3,
-              "mse_weight": 1})
+              "learning_rate": 0.001,
+              "epochs": 1000,
+              "batch_size": 32,
+              "kernel_size": 3})
 
 config = wandb.config
 
+# Store in dataloaders
+train_dataloader = DataLoader(x_windows, batch_size=config.batch_size, shuffle=True)
+test_dataloader = DataLoader(x_windows, batch_size=1, shuffle=False)
+
 print("Setting up coordinate VAE model...")
-encoder = Encoder(input_dim=1, latent_dim=1, kernel_size=config.kernel_size, num_layers=2, pool_step=2, device=device)
-decoder = Decoder(latent_dim=1, output_dim=1, kernel_size=config.kernel_size, num_layers=2, pool_step=2, device=device)
-model = CoordinateVAEModel(Encoder=encoder, Decoder=decoder)
+encoder = Encoder(input_dim=samples, 
+                  latent_dim=100, 
+                  kernel_size=config.kernel_size, 
+                  num_layers=3, 
+                  pool_step=4, 
+                  batch_size=config.batch_size, 
+                  device=device)
+decoder = Decoder(latent_dim=100, 
+                  output_dim=samples, 
+                  kernel_size=config.kernel_size, 
+                  num_layers=3, 
+                  pool_step=4, 
+                  device=device)
+model = CoordinateVAEModel(Encoder=encoder, 
+                           Decoder=decoder)
+
+# # -- View model
+# summary(model.to(device), [(2, samples)], 1)
 
 # --- Hyperparameter setup
 mse_loss = nn.MSELoss()
-kld_loss = nn.KLDivLoss()
+# kld_loss = nn.KLDivLoss()
 
-def loss_function(x, x_hat, mse_weight=0.25):
-    kld_weight = 1 - mse_weight
+def loss_function(x, x_hat, q_y, categorical_dim, kld_weight):
 
-    mse = mse_loss(x, x_hat)
-    kld = kld_loss(F.log_softmax(x, -1), F.softmax(x_hat, -1))
+    MSE = mse_loss(x, x_hat)
+    # kld = kld_loss(F.log_softmax(x, -1), F.softmax(x_hat, -1))
 
-    return (mse_weight * mse) + (kld_weight * -kld)
+    log_ratio = torch.log(q_y * categorical_dim + 1e-20)
+    KLD = torch.sum(q_y * log_ratio, dim=-1).mean()
+    # print("KLD was: ", KLD)
+
+    MSE = (1 - kld_weight) * MSE
+    KLD = kld_weight * KLD
+    
+    # return (mse_weight * mse) + (kld_weight * -kld)
+    return MSE + KLD, KLD
 
 
-optimizer = Adam(model.parameters(), lr=config.learning_rate, weight_decay=1e-5)
+optimizer = AdamW(model.parameters(), 
+                  lr=config.learning_rate)
 wandb.watch(model, log="all")
 
 # --- Training - 1 epoch?
@@ -111,10 +135,14 @@ if torch.cuda.is_available():
     model.cuda()
 
 best_loss = 99999.0
+kld_weight = 0.001
+kld_rate = 2 # TODO: Change this to rate of increase as per:
+                      # https://arxiv.org/pdf/1511.06349.pdf
+kld_tracked = []
 
 for epoch in range(config.epochs):
     overall_loss = 0
-    model.epoch = epoch + 1
+    # model.epoch = epoch + 1
 
     for batch_idx, data in enumerate(train_dataloader):
         data = data.to(device).float()
@@ -122,24 +150,39 @@ for epoch in range(config.epochs):
         
         optimizer.zero_grad()
 
-        x_hat = model(data)
-        loss = loss_function(data, x_hat, mse_weight=config.mse_weight)
-        
+        x_hat, q_y, categorical_dim = model(data)
+        loss, kld = loss_function(data, x_hat, q_y, categorical_dim, kld_weight=kld_weight)
+        kld_tracked.append(kld.detach().cpu())
+
         overall_loss += loss.item()*len(data)
         
         loss.backward()
-
-        # # Uncomment to print out model parameters (check if gradients are there)
-        # for param in model.parameters():
-        #     print(param.grad)
             
         optimizer.step()
+
+        if kld_weight < 1:
+            kld_weight *= kld_rate
+
+        if kld_weight > 1:
+            kld_weight = 1
+
+        # if (epoch % 99 == 0) and (batch_idx == 0):
+        #     # # Uncomment to print out model parameters (check if gradients are there)
+        #     # for param in model.parameters():
+        #     #     print(param.grad)
+
+        #     x_hat = x_hat.detach().cpu().numpy()
+        #     plt.plot(x_hat[0, 0, :])
+        #     plt.plot(x_hat[0, 1, :])
+        #     plt.show(block=False)
+        #     plt.pause(3)
+        #     plt.close()
 
         if loss < best_loss:
             best_model = copy.deepcopy(model)
             
-    print("\tEpoch", epoch + 1, "complete!", "\tAverage Loss: ", overall_loss / (batch_idx*8))
-    wandb.log({"loss": overall_loss / (batch_idx*8)})
+    print("\tEpoch", epoch + 1, "complete!", "\tAverage Loss: ", overall_loss / (batch_idx*config.batch_size))
+    wandb.log({"loss": overall_loss / (batch_idx*config.batch_size)})
         
 print("Finished!")
 
@@ -152,7 +195,7 @@ with torch.no_grad():
     # onehots = np.zeros((len(x_windows), int(samples/4)))
     for idx, test_data in enumerate(test_dataloader):
         test_data = test_data.to(device).float()
-        x_hat, onehot = best_model(test_data)
+        x_hat, q_y, categorical_dim = best_model(test_data)
 
         # Convert to numpy and send to cpu
         x_hat = x_hat.cpu().numpy()
@@ -167,4 +210,7 @@ plt.plot(x_delayed_flat, label="delayed sine")
 plt.plot(x_hats[:, 0, :].flatten(), label="reconstructed original sine")
 plt.plot(x_hats[:, 1, :].flatten(), label="reconstructed delayed sine")
 plt.legend()
+plt.show()
+
+plt.plot(kld_tracked)
 plt.show()
