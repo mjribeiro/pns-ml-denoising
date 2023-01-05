ab4774f553194f8547faf23bddddbb4ed14cb357
Mafalda <me@mafalda.dev>
2023-01-05 10:48:52 +0000

    WIP plot all channels for paper

--- plot_sample.py
+++ plot_sample.py
@@ -1,10 +1,13 @@
 from torch.utils.data import DataLoader
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 
 # Temp
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import numpy as np
 
 # Local imports
-from models.vae_model import *
+from models.cvae_model import *
 from datasets.vagus_dataset import VagusDataset
 
 # Load vagus dataset
@@ -14,7 +17,14 @@ test_dataset  = VagusDataset(train=False)
 train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=8, shuffle=True)
 
-sample = train_dataset.__getitem__(20)[0]
+sample = train_dataset.__getitem__(5)
 
-plt.plot(sample)
+fig, ax = plt.subplots(9)
+colors = plt.cm.Set1.colors
+
+for idx, color in zip(range(np.shape(sample)[0]), colors):
+    ax[idx].plot(sample[idx], color=color)
+    ax[idx].axis('off')
+
+ax[idx].axis('on')
 plt.show()
